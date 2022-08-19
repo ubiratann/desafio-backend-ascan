9a767901acc06af4e40078c5f2e33ea8d0b2d108
Ubiratan <r14.ubiratanjunior@gmail.com>
2022-08-19 06:53:58 -0300

    Adicona campos de status ao schema

--- producer/api/schema/subscription.py
+++ producer/api/schema/subscription.py
@@ -3,5 +3,7 @@ from marshmallow import Schema, fields
 class CreateSubscriptionSchema(Schema):
     user_id    = fields.Int(required=True)
 
-class UpdateSubscriptionSchema(Schema):
-    id = fields.Number(required=True)
+class UpdateSubscriptionStatusSchema(Schema):
+    subscription_id = fields.Number(required=True)
+    canceled        = fields.Bool(required=False, default=False)
+    restarted       = fields.Bool(required=False, default=False)
